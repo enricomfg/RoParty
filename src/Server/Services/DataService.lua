842443fdf6b9cba1e1a3cfdbf65a69881e74dea9
Enrico Machado <enricomfg@gmail.com>
2021-03-17 09:54:01 -0300

    Deleted tutorial services

---
+++ src/Server/Services/DataService.lua
@@ -0,0 +1,20 @@
+-- Data Service
+-- RicoFox
+-- March 17, 2021
+
+
+
+local DataService = {Client = {}}
+
+
+function DataService:Start()
+	
+end
+
+
+function DataService:Init()
+	
+end
+
+
+return DataService
